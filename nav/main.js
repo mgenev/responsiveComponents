6ecc4639948d212a72255f8e4d51ac7dcf894463
Martin Genev <mgenev@Martins-MacBook-Pro.local>
2013-07-14 19:42:06 -0700

    moving to folders

---
+++ nav/main.js
@@ -0,0 +1,12 @@
+$(document).ready(function() {
+	$('body').addClass('js');
+	var $menu = $('#menu'),
+	$menulink = $('.menu-link'),
+	$wrap = $('#wrap');
+
+	$menulink.click(function() {
+		$menulink.toggleClass('active');
+		$wrap.toggleClass('active');
+		return false;
+	});
+});
